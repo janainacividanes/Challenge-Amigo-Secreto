230815297215906248f7396dfac332a24eebf22f
janainacividanes <jana_cividanes@hotmail.com>
2025-01-25 16:57:31 -0300

    Primeiros passos

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+    if (nomeAmigo == "") {
+        alert ("Por favor, insira um nome.");
+    } else {
+        amigos.push(nomeAmigo);
+        console.log(amigos);
+        atualizarLista();
+        limparCampos();
+    }
+}
+
+function limparCampos() {
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = "";
+}
+
+function atualizarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (const amigo of amigos) {
+        const item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    }
+}
+
+
